5ae197456b5a11cc40c3a2776004ad8aae313d83
kalazus <kalazus@mail.ru>
2024-03-19 17:23:08 +0300

    override requirements, install in docker

--- build/COPY_ROOT/opt/ai-dock/bin/build/layer2/init.sh
+++ build/COPY_ROOT/opt/ai-dock/bin/build/layer2/init.sh
@@ -12,5 +12,12 @@ build_common_clone_fooocus() {
     git clone ${fooocus_git}
 }
 
+install_requirements() {
+    cd /opt/Fooocus
+    sed -i 's/==/>=/g' requirements_versions.txt
+    micromamba run -n webui ${PIP_INSTALL} -r requirements_versions.txt
+}
+
 build_common_clone_fooocus
+install_requirements
 fix-permissions.sh -o container
